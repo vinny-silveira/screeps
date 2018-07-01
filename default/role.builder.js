71171e0542da5cffc88d37bed997999c1fe11898
vmoura-vogel <vinicius.silveira@vogeltelecom.com>
2018-07-01 18:23:52 -0300

    Added 'parking' and fixes

--- default/role.builder.js
+++ default/role.builder.js
@@ -11,27 +11,31 @@ let roleBuilder = {
         let sourceKey       = params.defaultSource.builder;
         let constructions   = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
         let sources         = creep.room.find(FIND_SOURCES);
+        let parking         = creep.room.find(FIND_FLAGS);
+
         let harvest         = (creep.memory.building && creep.carry.energy === 0);
         let build           = (!creep.memory.building && creep.carry.energy === creep.carryCapacity);
+        let park            = (creep.carry.energy <= creep.carryCapacity && constructions === null);
 
         // Prevent access to a source that not exist...
         if (sources.length === 1) {
             sourceKey = 0;
         }
 
-        if (build) {
+        if (build && constructions !== null) {
             creep.memory.building = true;
         }
-        if (harvest) {
+
+        if (harvest || park) {
             creep.memory.building = false;
         }
 
         if (creep.memory.building) {
-            if (constructions) {
-                prototypeCreep.creepBuild(creep, constructions);
-            }
-        } else {
+            prototypeCreep.creepBuild(creep, constructions);
+        } else if (creep.carry.energy < creep.carryCapacity) {
             prototypeCreep.creepHarvest(creep, sources[sourceKey]);
+        } else if (parking.length) {
+            prototypeCreep.creepMove(creep, parking[0]);
         }
     }
 };
